324a3f05e483bd2f10882766dec3de8cf6733ce6
wuhaoran111git config --global user.name wuhaoran111 <592651525@qq.com>
2021-02-25 20:35:06 +0800

    Updating

--- raw_data/data_2018-01-28-15-00-12/n0_pre_sync.py
+++ raw_data/data_2018-01-28-15-00-12/n0_pre_sync.py
@@ -7,14 +7,14 @@ image_list = []
 for image_file in os.listdir('./image'):
     image_list.append(image_file[0:19])
 image_list = sorted(image_list)
-image_time_list = map(lambda x: int(x), image_list)
+image_time_list = list(map(lambda x: int(x), image_list))
 image_time = np.array(image_time_list) * 1e-9
 
 pc_list = []
 for pc_file in os.listdir('./pcl1'):
     pc_list.append(pc_file[0:10] + pc_file[11:20])
 pc_list = sorted(pc_list)
-pc_time_list = map(lambda x: int(x), pc_list)
+pc_time_list = list(map(lambda x: int(x), pc_list))
 pc_time = np.array(pc_time_list) * 1e-9
 
 sync_list = []
